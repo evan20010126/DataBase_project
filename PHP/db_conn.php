54ae4bf14a194949cb80654d95f419092936a3b8
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-06 03:46:39 +0800

    Update db_conn.php

--- PHP/db_conn.php
+++ PHP/db_conn.php
@@ -2,8 +2,10 @@
     $user = 'root'; //資料庫使用者名稱
     $password = 'eeeeee'; //資料庫的密碼
     try{
-        $db = new 
+        $db = new
+            //?? 
             PDO('mysql:host=127.0.0.1;dbname=mytest;charset=utf8',$user,$password);
+            //??
         //之後若要結束與資料庫的連線，則使用「$db = null;」
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false); //強制預處理 false
