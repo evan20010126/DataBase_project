77364ed84870a2fadc0793d0d68f317c19db81aa
adam9876543210 <86069188+adam9876543210@users.noreply.github.com>
2022-01-08 05:51:19 +0800

    Update delete_buyer.php

--- PHP/delete_buyer.php
+++ PHP/delete_buyer.php
@@ -1,11 +1,17 @@
 <?php   /*記得第五行 ['BuyerID'] 改成由前端網頁輸入*/
     include "db_conn.php";
     
-    $query = ("DELETE FROM Buyer 
-               WHERE BuyerID = [BuyerID]"); //[BuyerID]依使用者輸入, 型態為int
-
+    $BuyerID = $_REQUEST["a"]; //Line4 前端要同步
+    try{
+    $query = ("DELETE FROM Buyer WHERE BuyerID = ?"); 
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(); //執行sql語法
-    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
-    echo json_encode($result); //回傳json格式
+    $error = $stmt->execute(array($BuyerID)); //執行sql語法
+    echo json_encode(1); 
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //若新增成功輸出1
+    //若新增失敗輸出空陣列
+
 ?>
