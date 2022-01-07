5a653227edeb385398784aca0899f8cf99bbb322
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 04:39:37 +0800

    Join 版本

--- PHP/popular_storage.php
+++ PHP/popular_storage.php
@@ -2,8 +2,8 @@
     include "db_conn.php";
     
     $query = ("SELECT BookName 
-	FROM deal,storage 
-    WHERE storage.BookID=deal.BookID 
+	FROM deal 
+    NATURAL JOIN storage 
 	GROUP BY BookName
 	ORDER BY sum(DealAmount) desc");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
@@ -11,3 +11,4 @@
     $result = $stmt->fetchAll();   //將所有搜尋結果存於result
     echo json_encode($result); //回傳json格式
 ?>
+
