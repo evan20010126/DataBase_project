e06fd0053c24cc43df2df8a81d20cc2936a54e21
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 01:49:01 +0800

    display_all_book_new
    //輸出{"BookID,"BookName","Price","Classification","StoreNumber"}}(依此排序輸出)

---
+++ PHP/display_all_books.php
@@ -0,0 +1,10 @@
+<?php
+    include "db_conn.php";
+    
+    $query = ("SELECT * FROM storage");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(); //執行sql語法
+    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+    echo json_encode($result); //回傳json格式
+    //輸出{"BookID,"BookName","Price","Classification","StoreNumber"}}(依此排序輸出)
+?>
