69a134912d6c834dba94f9f76b79300f4b441eaa
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 03:56:04 +0800

    Update edit_buyer_information.php
    //若執行成功輸出1
        //若執行失敗輸出空陣列

--- PHP/edit_buyer_information.php
+++ PHP/edit_buyer_information.php
@@ -1,12 +1,19 @@
-<?php   /*記得第五行['BuyerID'] 改成由前端網頁輸入*/
+<?php       /*記得第五行[DealID] 改成由前端網頁輸入*/
     include "db_conn.php";
+    $Attribute=$_REQUEST["a"];
+    $value=$_REQUEST["b"];
+    $BuyerID=$_REQUEST["c"];
     
-    $query = ("UPDATE buyer
-               SET BuyerID = [BuyerID]            /*Foreign(deal.BuyerID) and Parent(buyer.BuyerID)  can work!!!*/
-               WHERE BuyerPhone = [BuyerPhone]"); //[BuyerPhone]依使用者輸入, 型態為varchar
-
-    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(); //執行sql語法
-    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
-    echo json_encode($result); //回傳json格式
+    $query = ("UPDATE buyer SET " . strval($Attribute) . " =  ? WHERE BuyerID = ?"); 
+    try{
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($value,$BuyerID)); //執行sql語法
+        $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+        echo json_encode(1); //回傳json格式
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //若執行成功輸出1
+    //若執行失敗輸出空陣列
 ?>
