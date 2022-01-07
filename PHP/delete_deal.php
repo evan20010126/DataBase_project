87a1b1feddc3568f1a56f2f347898124dd3e5d31
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 03:08:56 +0800

    Update delete_deal.php

--- PHP/delete_deal.php
+++ PHP/delete_deal.php
@@ -6,7 +6,7 @@
         $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
         $error = $stmt->execute(array($DealID)); //執行sql語法
         $result = $stmt->fetchAll();   //將所有搜尋結果存於result
-        echo json_encode(array(1)); //回傳json格式
+        echo json_encode(1); //回傳json格式
     }
     catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
         echo json_encode(array());
