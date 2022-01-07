ecc888e5264acb797614afeefbdd5b04c0bed176
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 05:53:59 +0800

    Update delete_buyer.php

--- PHP/delete_buyer.php
+++ PHP/delete_buyer.php
@@ -1,6 +1,5 @@
 <?php   /*記得第五行 ['BuyerID'] 改成由前端網頁輸入*/
     include "db_conn.php";
-    
     $query = ("DELETE FROM Buyer 
                WHERE BuyerID = [BuyerID]"); //[BuyerID]依使用者輸入, 型態為int
 
@@ -8,4 +7,5 @@
     $error = $stmt->execute(); //執行sql語法
     $result = $stmt->fetchAll();   //將所有搜尋結果存於result
     echo json_encode($result); //回傳json格式
+    
 ?>
