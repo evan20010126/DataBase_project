8081d79f744276a2ffdc1de0a8e3d8bc4f4eef16
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 03:35:18 +0800

    Update edit_deal_information.php

--- PHP/edit_deal_information.php
+++ PHP/edit_deal_information.php
@@ -1,12 +1,19 @@
 <?php       /*記得第五行[DealID] 改成由前端網頁輸入*/
     include "db_conn.php";
+    $Attribute=$_REQUEST["a"];
+    $value=$_REQUEST["b"];
+    $DealID=$_REQUEST["c"];
     
-    $query = ("UPDATE Deal
-               SET [Attribute] =  [value]  /*[Attribute]與[value]依使用者輸入*/
-               WHERE DealID = [DealID]"); //[DealID]依使用者輸入
-
-    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(); //執行sql語法
-    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
-    echo json_encode($result); //回傳json格式
+    $query = ("UPDATE Deal SET " . strval($Attribute) . " =  ? WHERE DealID = ?"); 
+    try{
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($value,$DealID)); //執行sql語法
+        $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+        echo json_encode(1); //回傳json格式
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //若執行成功輸出1(包含原本不在的東西刪除等同於沒動作)
+    //若執行失敗輸出空陣列
 ?>
