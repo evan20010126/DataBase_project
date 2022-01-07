1ba391b5d1b2dbe690cac770004cbdfaa9146b67
YiKaiWu1125 <94746828+YiKaiWu1125@users.noreply.github.com>
2022-01-07 17:31:08 +0800

    Update Storage_Classification.php

--- PHP/Storage_Classification.php
+++ PHP/Storage_Classification.php
@@ -1,7 +1,7 @@
 <?php
     include "db_conn.php";
     
-    $query = ("SELECT Classification FROM storage");
+    $query = ("SELECT distinct Classification FROM storage");
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(); //執行sql語法
     $result = $stmt->fetchAll();   //將所有搜尋結果存於result
