64901d40ef9b0b36bb1a8ede51bf4fad85fd2b50
adam9876543210 <86069188+adam9876543210@users.noreply.github.com>
2022-01-08 05:51:15 +0800

    Update add_new_deal.php

--- PHP/add_new_deal.php
+++ PHP/add_new_deal.php
@@ -1,11 +1,20 @@
 <?php       /*記得第五行 value(int,varchar,int,int) 改成由前端網頁輸入*/
     include "db_conn.php";
     
-    $query = ("INSERT INTO deal(DealID,BookName,BuyerID,DealAmount)
-               VALUES(NULL,'nuclear3000', 101, 20000)"); //value(int,varchar,int,int)
+    $BookID = $_REQUEST["a"]; //Line4 ~ Line6 前端要同步
+    $BuyerID = $_REQUEST["b"];
+    $DealAmount = $_REQUEST["c"];
+    
 
-    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(); //執行sql語法
-    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
-    echo json_encode($result); //回傳json格式
+    try{
+        $query = ("INSERT INTO deal(DealID,BookID,BuyerID,DealAmount) VALUES(NULL,?,?,?)"); //value(int,varchar,int,int)
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($BookID,$BuyerID,$DealAmount)); //執行sql語法
+        echo json_encode(1);
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //若新增成功輸出1
+    //若新增失敗輸出空陣列
 ?>
