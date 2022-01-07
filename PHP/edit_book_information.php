d48c504d56af5ae515df2e5a59ce7520a9910508
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 03:40:12 +0800

    Create edit_book_information.php

---
+++ PHP/edit_book_information.php
@@ -0,0 +1,19 @@
+<?php       /*記得第五行[DealID] 改成由前端網頁輸入*/
+    include "db_conn.php";
+    $Attribute=$_REQUEST["a"];
+    $value=$_REQUEST["b"];
+    $book_name=$_REQUEST["c"];
+    
+    $query = ("UPDATE storage SET " . strval($Attribute) . " =  ? WHERE BookName = ?"); 
+    try{
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($value,$book_name)); //執行sql語法
+        $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+        echo json_encode(1); //回傳json格式
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //若執行成功輸出1(更新原本不在的東西刪除等同於沒動作)
+    //若執行失敗輸出空陣列
+?>
