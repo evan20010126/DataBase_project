8f6cf25f27b958287e58493c859716d7ce4386f0
YiKaiWu1125 <500000000kai@gmail.com>
2022-01-08 01:59:51 +0800

    show_selected_classification_new
    //輸出{"BookID,"BookName","Price","Classification","StoreNumber"}}(依此排序輸出)
        //若查無則輸出空陣列

---
+++ PHP/show_selected_classification.php
@@ -0,0 +1,16 @@
+<?php
+    include "db_conn.php";
+    $Classification = $_REQUEST["a"];
+    try{
+        $query = ("SELECT * FROM storage WHERE Classification = ? ");
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(array($Classification)); //執行sql語法
+        $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+        echo json_encode($result); //回傳json格式
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode();
+    }
+    //輸出{"BookID,"BookName","Price","Classification","StoreNumber"}}(依此排序輸出)
+    //若查無則輸出空陣列
+?>
