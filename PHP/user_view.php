e4c49b98343beb738040f9399f47728cefe7a327
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-01-10 12:19:18 +0800

    火燒屁股啦

---
+++ PHP/user_view.php
@@ -0,0 +1,14 @@
+<?php
+    include "db_conn.php";
+    try{
+        $query = ("SELECT * FROM user_view;");
+        $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+        $error = $stmt->execute(); //執行sql語法
+        $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+        echo json_encode($result); //回傳json格式
+    }
+    catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
+        echo json_encode(array());
+    }
+    //輸出{"BookID,"BookName","Price","Classification","StoreNumber"}}(依此排序輸出)
+?>
