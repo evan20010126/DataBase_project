8941dba87a9c9329a884fd5dff0f273ec3134012
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-01-10 11:21:53 +0800

    Demo文件編輯，php檔案與html檔案調整成可以印出add_deal的錯誤訊息

--- PHP/add_new_book.php
+++ PHP/add_new_book.php
@@ -9,10 +9,10 @@
         $query = ("INSERT INTO storage(BookID,BookName,Price,Classification,StoreNumber) VALUES(NULL,?,?,?,?)");   
         $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
         $result = $stmt ->execute(array($BookName,$Price,$Classification,$StoreNumber));
-        echo json_encode(1);
+        echo "1";
     }
     catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
-        echo json_encode(array());
+        echo "error!" . $e->getMessage();
     }
     //若新增成功輸出1
     //若新增失敗輸出空陣列
--- PHP/add_new_deal.php
+++ PHP/add_new_deal.php
@@ -10,10 +10,10 @@
         $query = ("INSERT INTO deal(DealID,BookID,BuyerID,DealAmount) VALUES(NULL,?,?,?)"); //value(int,varchar,int,int)
         $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
         $error = $stmt->execute(array($BookID,$BuyerID,$DealAmount)); //執行sql語法
-        echo json_encode(1);
+        echo "1";
     }
     catch(Exception $e){ //若上述程式碼出現錯誤，便會執行以下動作
-        echo json_encode(array());
+        echo strval("error!\n" . $e->getMessage());
     }
     //若新增成功輸出1
     //若新增失敗輸出空陣列
