fb612e010618b9f001396a2915ef84baeaa7f0e5
evan <55246608+evan20010126@users.noreply.github.com>
2022-09-26 14:51:26 +0800

    database_project

--- PHP/db_conn.php
+++ PHP/db_conn.php
@@ -1,10 +1,12 @@
 <?php 
-    session_start();
-    $user =  $_SESSION['user']; //資料庫使用者名稱
-    $password = $_SESSION['password']; //資料庫的密碼
+    // session_start();
+    // $user =  $_SESSION['user']; //資料庫使用者名稱
+    $user =  "root"; //資料庫使用者名稱
+    // $password = $_SESSION['password']; //資料庫的密碼
+    $password = "123456789"; //資料庫的密碼
     try{
         $db = new 
-            PDO('mysql:host=20.212.218.184;dbname=ntou_bks;charset=utf8',$user,$password);
+            PDO('mysql:host=127.0.0.1;dbname=ntou_bks;charset=utf8',$user,$password);
         //之後若要結束與資料庫的連線，則使用「$db = null;」
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false); //強制預處理 false
