98555b94d7c206f8f8be91ca3faed4784208b4e9
evan <55246608+evan20010126@users.noreply.github.com>
2022-09-26 14:42:08 +0800

    ntou_bks mysql database backup

---
+++ ntou_bks.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 26, 2022 at 08:40 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ntou_bks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `BuyerID` bigint(15) NOT NULL,
+  `BuyerName` varchar(255) NOT NULL,
+  `BuyerPhone` varchar(10) NOT NULL,
+  `BuyerAddress` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `buyer`
+--
+
+INSERT INTO `buyer` (`BuyerID`, `BuyerName`, `BuyerPhone`, `BuyerAddress`) VALUES
+(1, 'yumi\r\n', '0988226379', '新竹縣尖石鄉玉峰村14鄰司馬庫斯2號'),
+(2, 'evan', '0963908097', '台北市松山區南京西路\r\n1段123號1234樓之7'),
+(3, 'edmund\r\n', '0933566974', '台北市松山區南京東路\r\n5段230號14樓之6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyerinfo`
+--
+
+CREATE TABLE `buyerinfo` (
+  `BuyerPhone` varchar(225) NOT NULL,
+  `BuyerName` varchar(225) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `buyerinfo`
+--
+
+INSERT INTO `buyerinfo` (`BuyerPhone`, `BuyerName`) VALUES
+('0972065555', 'Trump'),
+('0972066464', 'Xi'),
+('0972067777', 'Stanley');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deal`
+--
+
+CREATE TABLE `deal` (
+  `DealID` int(15) NOT NULL,
+  `BookID` varchar(225) NOT NULL,
+  `BuyerID` int(15) NOT NULL,
+  `DealAmount` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `deal`
+--
+
+INSERT INTO `deal` (`DealID`, `BookID`, `BuyerID`, `DealAmount`) VALUES
+(1, '3', 3, 100),
+(2, '2', 1, 1),
+(3, '1', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `storage`
+--
+
+CREATE TABLE `storage` (
+  `BookID` bigint(15) NOT NULL,
+  `BookName` varchar(255) NOT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Classification` varchar(255) DEFAULT NULL,
+  `StoreNumber` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `storage`
+--
+
+INSERT INTO `storage` (`BookID`, `BookName`, `Price`, `Classification`, `StoreNumber`) VALUES
+(1, 'Hamen', 150, '宗教類', 0),
+(2, 'How to Lose', 3000, '科學類', 1000),
+(3, '7414.ac', 888, '歷史類', 110),
+(6, 'hero1', 120, '歷史類', 100),
+(7, 'wwww', 123, 'network', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `user_view`
+-- (See below for the actual view)
+--
+CREATE TABLE `user_view` (
+`BookID` bigint(15)
+,`BookName` varchar(255)
+,`Classification` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `user_view`
+--
+DROP TABLE IF EXISTS `user_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_view`  AS SELECT `storage`.`BookID` AS `BookID`, `storage`.`BookName` AS `BookName`, `storage`.`Classification` AS `Classification` FROM `storage``storage`  ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD PRIMARY KEY (`BuyerID`);
+
+--
+-- Indexes for table `buyerinfo`
+--
+ALTER TABLE `buyerinfo`
+  ADD PRIMARY KEY (`BuyerPhone`);
+
+--
+-- Indexes for table `deal`
+--
+ALTER TABLE `deal`
+  ADD PRIMARY KEY (`DealID`);
+
+--
+-- Indexes for table `storage`
+--
+ALTER TABLE `storage`
+  ADD PRIMARY KEY (`BookID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buyer`
+--
+ALTER TABLE `buyer`
+  MODIFY `BuyerID` bigint(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `storage`
+--
+ALTER TABLE `storage`
+  MODIFY `BookID` bigint(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
